bbce4143ab54d8c19e6c9282abdb16ca1451cfeb
Christian Pardo <chrispardopersonal@gmail.com>
2020-09-29 11:42:47 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,18 @@
+class Board 
+  
+  attr_accessor :cells
+  
+  def initialize()
+    reset!
+  end 
+  
+  def reset! 
+    @cells = Array.new(9, " ")
+  end 
+  
+  def display
+    
+  end 
+  
+  
+end 
